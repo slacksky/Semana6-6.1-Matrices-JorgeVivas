c803bd86d89a9c2eb6d7a4d9cdcb05266b19f329
Jorge L. Vivas C <slacksky@users.noreply.github.com>
2021-06-14 13:45:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+""" Programa lineas de metro
+    Programa para el manejo de conexiones entre estaciones de metro
+    incorpora al modulo rostros.py
+    Oscar Franco-Bedoya
+    Junio 2-2021 """
+
+#---------------- Zona librerias------------
+import metro as mt
+
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+
+#----------Definición de Funciones (Dividir)------------
+
+#======================================================================
+#   Algoritmo principal Punto de entrada a la aplicación (Conquistar)
+# =====================================================================
---
+++ metro.py
@@ -0,0 +1,28 @@
+""" Modulo ModuleNotFoundError
+    Funciones para el manejo de las lineas de metro
+    con matrices
+    Oscar Franco-Bedoya
+    Mayo 10-2021 """
+# Definición de Funciones
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+def leer_numero_estaciones():
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def leer_conexiones(numero_estaciones):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def obtener_lista_conexiones_estacion(matriz_conexiones,numero_estacion):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
+
+def obtener_wstacion_mas_conectada(matriz_conexiones):
+  #TODO Comentar código
+  #TODO Implementar la función
+  return "No implementada aún"
